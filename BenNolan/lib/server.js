a0026bc50c99b1550695b3cc8b6b834be90cb994
bonecrushereb <bonecrushereb@gmail.com>
2016-04-12 11:40:01 -0700

    gulp is passing while server is being setup

--- BenNolan/lib/server.js
+++ BenNolan/lib/server.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 const server = net.createServer((socket) => {
   socket.pipe(process.stdout);
 
-  socket.on('data', (data) => {
-    const writeToFile = fs.createWriteStream(__dirname + '/logs/log_' + Date.now() + '.txt')
+  socket.on('data', () => {
+    const writeToFile = fs.createWriteStream(__dirname + '/logs/log_' + Date.now() + '.txt');
     socket.pipe(writeToFile);
     socket.end('request complete');
   });
 });
 
 server.listen(3000, () => {
-  process.stdout.write('server up on 3000\n')
+  process.stdout.write('server up on 3000\n');
 });
+
+exports.server = server;
--- BenNolan/test/tcp_server_test.js
+++ BenNolan/test/tcp_server_test.js
@@ -1,27 +1,27 @@
-const chai = require('chai')
+const chai = require('chai');
 const expect = chai.expect;
-const fs = require('fs');
 const net = require('net');
-const server = require(__dirname + '/../lib/server');
+const tcp = require(__dirname + '/../lib/server');
 
 describe('Test tcp server', () => {
+  it('Should recieve a response from server', (done) => {
 
-    it('Should recieve a response from server', function (done) {
-
-        var client = net.connect({ port: 3000 },
-            function() {
-                client.write('Let\'s send this data!\n');
+    var client = net.connect({ port: 3000 },
+            () => {
+              client.write('Let\'s send this data!\n');
             });
 
-        client.on('data', function(data) {
-            expect(data.toString()).to.equal('request complete');
-            client.end();
-        });
-
-        client.on('end', () => {
-          console.log('disconnected from server!');
-          done();
-        });
+    client.on('data', (data) => {
+      expect(data.toString()).to.equal('request complete');
+      client.end();
     });
 
+    client.on('end', () => {
+      console.log('disconnected from server!');
+      done();
+    });
+  });
+  after(() => {
+    tcp.server.close();
+  });
 });
