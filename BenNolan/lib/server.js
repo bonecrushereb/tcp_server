fe78b08638c7e6ca1101626e9a54e3d045e8823c
bonecrushereb <bonecrushereb@gmail.com>
2016-04-13 12:48:22 -0700

    able to write to folder. ready for resubmit

--- BenNolan/lib/server.js
+++ BenNolan/lib/server.js
@@ -2,14 +2,10 @@ const net = require('net');
 const fs = require('fs');
 
 const server = net.createServer((socket) => {
-  socket.pipe(process.stdout);
-
-  socket.on('data', () => {
-    const writeToFile = fs.createWriteStream(__dirname + '/logs/log_' + Date.now() + '.txt');
-    socket.pipe(writeToFile);
-    socket.end('request complete');
-  });
-});
+    const wToFile = fs.createWriteStream(__dirname + '/logs/log_' + Date.now() + '.txt');
+    socket.pipe(wToFile);
+      socket.end('request complete\n');
+    });
 
 server.listen(3000, () => {
   process.stdout.write('server up on 3000\n');
